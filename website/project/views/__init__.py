eb19e92d37919eb709a1c53fdf7b2c52c7ea6388
rand-honda <jyunji.honda@pmb.rand.co.jp>
2024-01-12 19:00:03 +0900

    未病データベース構築として以下の機能を追加しました -> ①ユーザー定義メタデータ編集画面の追加、②HDF5作成機能の追加、③ユーザー定義メタデータ インポート／エクスポート機能の追加

--- website/project/views/__init__.py
+++ website/project/views/__init__.py
@@ -1,1 +1,1 @@
-from . import contributor, node, register, tag, file, comment, drafts, timestamp, quota  # noqa
+from . import contributor, node, register, tag, file, comment, drafts, timestamp, quota, hdf5  # noqa
---
+++ website/project/views/hdf5.py
@@ -0,0 +1,4 @@
+import sys
+
+def makeHDF5():
+    return 'okjj'
